df58496796c47e4f072792566c4341a8668d2b63
Brian Hestir <brhestir@gmail.com>
2021-01-19 09:41:28 -0500

    Added express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("api/test/", (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost${PORT}`);
+});
