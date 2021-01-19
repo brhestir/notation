86756541b413c1885cef47f8663fbcf6ce4af7be
Brian Hestir <brhestir@gmail.com>
2021-01-19 09:48:18 -0500

    Added path include

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("api/test/", (req, res) => {
