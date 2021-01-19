43d6d55bafc2a080a2b980c787b77a9009e541dd
Brian Hestir <brhestir@gmail.com>
2021-01-19 09:56:33 -0500

    Corrected typo

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
